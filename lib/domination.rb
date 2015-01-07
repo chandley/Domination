37feb5a2f1d415fd2658ad7b0e0c6de4ea5c1f65
Pavel Redics <pavel.redics@gmail.com>
2015-01-07 18:29:44 +0000

    setup europe map

--- lib/domination.rb
+++ lib/domination.rb
@@ -3,8 +3,15 @@ require 'sinatra/base'
 require_relative "./setup_game.rb"
 
 class Domination < Sinatra::Base
+
+  set :public_folder, File.join(root, "..", "public") 
+
   get '/' do
     erb :game
+  end
+
+  get '/map' do
+    erb :map
   end
 
   # start the server if ruby file executed directly
