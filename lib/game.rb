ac4052c7a0b32e195387ef488f0c27faffca0093
Shan Hasan <shan119@hotmail.co.uk>
2015-01-08 11:37:30 +0000

    updated adding player two test

--- lib/game.rb
+++ lib/game.rb
@@ -7,13 +7,13 @@ class Game
   end
 
   def add_player(player)
-    self.player1 ? self.player2 = player : self.player1 = player unless has_to_players
+    self.player1 ? self.player2 = player : self.player1 = player unless has_two_players?
   end
 
   private
 
-  def has_to_players
-  	!self.player2 = nil
+  def has_two_players?
+    !player2.nil?
   end
 
 
