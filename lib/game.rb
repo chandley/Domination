dacf2edc0dc2805ce32195a73f28fd8445b4e797
langesi <langesi@aol.de>
2015-01-08 11:31:01 +0000

    added players and test for players

--- lib/game.rb
+++ lib/game.rb
@@ -7,6 +7,15 @@ class Game
   end
 
   def add_player(player)
-    self.player1 = player
+    self.player1 ? self.player2 = player : self.player1 = player unless has_to_players
   end
+
+  private
+
+  def has_to_players
+  	!self.player2 = nil
+  end
+
+
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,12 +4,23 @@ describe Game do
 
   let (:game) {Game.new}
   let (:player1) {double :player}
+  let (:player2) {double :player}
  
   context "The basics" do
 
     it "can have a player added" do
       game.add_player(player1)
       expect(game.player1).to eq player1
+    end
+
+    it "can have a second player added" do
+    	game.add_player(player1)
+    	game.add_player(player2)
+    	expect(game.player2).to eq player2
+    end
+
+    it "can only have a second player if first player got added" do
+    	expect{game.add_player(player2)}.to raise_error("Add the first player first")
     end
 
   end
