f7c5b251dce996442bd996f62368230b9dab6599
Pavel Redics <pavel.redics@gmail.com>
2015-01-09 12:05:38 +0000

    we can attack now

--- lib/country.rb
+++ lib/country.rb
@@ -13,7 +13,7 @@ class Country
   end
 
   def remove_army
-    raise "Game error: There are no armies on this country" if army_count == 1 
+    raise "Game error: There are no armies on this country" if army_count == 0
     @armies -= 1
   end
 
--- lib/domination.rb
+++ lib/domination.rb
@@ -64,13 +64,14 @@ class Domination < Sinatra::Base
   end
 
   post '/attack' do
-    attacking_country, defending_country = params[:"attack country"], params[:"defending country"]
+    attacking_country = GAME.player1.countries.select { |c| c.name == params[:"attacking country"] }.first
+    defending_country = GAME.player2.countries.select { |c| c.name == params[:"defending country"] }.first
     battle_setup_hash = {:attacking_country => attacking_country,
                          :defending_country => defending_country,
-                         :attacking_player => @my_game.player1,
-                         :defending_player => @my_game.player2
+                         :attacking_player => GAME.player1,
+                         :defending_player => GAME.player2
                         }
-    @my_game.attack(battle_setup_hash)
+    GAME.attack(battle_setup_hash)
     redirect to '/map'
   end
 
